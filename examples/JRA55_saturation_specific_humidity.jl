18899e7ab2b09e6140ef6ac392f2481d129d5f61
Gregory Wagner <wagner.greg@gmail.com>
2025-04-25 14:11:24 -0600

    saturation adj

--- examples/JRA55_saturation_specific_humidity.jl
+++ examples/JRA55_saturation_specific_humidity.jl
@@ -1,4 +1,5 @@
 using JLD2
+using AquaSkyLES
 using AquaSkyLES: density, saturation_specific_humidity, MoistAuxiliaryState, AtmosphereThermodynamics
 using GLMakie
 
@@ -6,8 +7,28 @@ using GLMakie
 
 thermo = AtmosphereThermodynamics()
 
-A = @. MoistAuxiliaryState(p, T, q)
-ρ = density.(A, Ref(thermo))
-q★ = saturation_specific_humidity.(A, Ref(thermo))
+ρ = density.(T, p, q, Ref(thermo))
+q★ = saturation_specific_humidity.(T, 1.2, Ref(thermo))
+ql = @. max(0, q - q★)
 
-scatter(q★, T, color=ρ, colormap=:blues)
+fig = Figure(size=(1200, 600))
+ax1 = Axis(fig[1, 2], title="Temperature")
+ax2 = Axis(fig[2, 1], title="Saturation specific humidity")
+ax3 = Axis(fig[1, 1], title="Total specific humidity")
+ax4 = Axis(fig[2, 2], title="Liquid specific humidity")
+heatmap!(ax1, T, colormap=:magma)
+heatmap!(ax2, q★)
+heatmap!(ax3, q, colormap=:grays)
+heatmap!(ax4, ql, colormap=:grays, colorrange=(0, 0.001))
+display(fig)
+
+# Compute cloudiness for instantaneous drop
+θ⁻ = T .- 2
+Ψ = AquaSkyLES.MoistState{Float64}.(ρ, θ⁻, q, 0)
+ℛ = AquaSkyLES.ReferenceState{Float64}(101325, 20)
+T⁻ = AquaSkyLES.temperature.(Ψ, Ref(ℛ), Ref(thermo))
+Π = AquaSkyLES.exner_function.(Ψ, Ref(ℛ), Ref(thermo))
+Tu = @. Π * θ⁻
+ΔT = T⁻ - Tu
+
+heatmap!(ax1, ΔT, colormap=:magma)
--- src/atmospheric_thermodynamics.jl
+++ src/atmospheric_thermodynamics.jl
@@ -35,6 +35,7 @@ end
 
 struct AtmosphereThermodynamics{FT, C}
     molar_gas_constant :: FT
+    gravitational_acceleration :: FT
     dry_air :: IdealGas{FT}
     vapor :: IdealGas{FT}
     condensation :: C
@@ -42,6 +43,7 @@ end
 
 """
     AtmosphereThermodynamics(FT = Oceananigans.defaults.FloatType;
+                             gravitational_acceleration = 9.81,
                              molar_gas_constant = 8.314462618,
                              dry_air_molar_mass = 0.02897,
                              dry_air_isentropic_exponent = 2/7,
@@ -54,6 +56,7 @@ in Earth's atmosphere and water vapor. The default `isnothing(condensation)` imp
 appropriate for unsaturated and therefore non-condensing air.
 """
 function AtmosphereThermodynamics(FT = Oceananigans.defaults.FloatType;
+                                  gravitational_acceleration = 9.81,
                                   molar_gas_constant = 8.314462618,
                                   dry_air_molar_mass = 0.02897,
                                   dry_air_isentropic_exponent = 2/7,
@@ -68,6 +71,7 @@ function AtmosphereThermodynamics(FT = Oceananigans.defaults.FloatType;
                          isentropic_exponent = vapor_isentropic_exponent)
 
     return AtmosphereThermodynamics(convert(FT, molar_gas_constant),
+                                    convert(FT, gravitational_acceleration),
                                     dry_air, vapor, condensation)
 end
 
@@ -78,30 +82,32 @@ end
 const AT = AtmosphereThermodynamics
 const IG = IdealGas
 
-@inline gas_heat_capacity(R, ig::IG) = ig.isentropic_exponent * R / ig.molar_mass
+@inline gas_heat_capacity(R, ig::IG)          = ig.isentropic_exponent * R / ig.molar_mass
 @inline dry_air_heat_capacity(td::AT)         = gas_heat_capacity(td.molar_gas_constant, td.dry_air)
-@inline vapor_specific_heat_capacity(td::AT)  = gas_heat_capacity(td.molar_gas_constant, td.vapor)
-@inline vapor_specific_gas_constant(td::AT)   = td.molar_gas_constant / td.vapor.molar_mass
-@inline dry_air_specific_gas_constant(td::AT) = td.molar_gas_constant / td.dry_air.molar_mass
+@inline vapor_heat_capacity(td::AT)           = gas_heat_capacity(td.molar_gas_constant, td.vapor)
+@inline vapor_gas_constant(td::AT)            = td.molar_gas_constant / td.vapor.molar_mass
+@inline dry_air_gas_constant(td::AT) = td.molar_gas_constant / td.dry_air.molar_mass
 
 const NonCondensingAtmosphereThermodynamics{FT} = AtmosphereThermodynamics{FT, Nothing} 
 
 @inline function saturation_vapor_pressure(T, thermodynamics_constants)
+    @show T
     ℒ₀  = thermodynamics_constants.condensation.reference_vaporization_enthalpy
     T₀  = thermodynamics_constants.condensation.energy_reference_temperature
     Tᵗʳ = thermodynamics_constants.condensation.triple_point_temperature
     pᵗʳ = thermodynamics_constants.condensation.triple_point_pressure
     cᵖℓ = thermodynamics_constants.condensation.liquid_heat_capacity
-    cᵖv = vapor_specific_heat_capacity(thermodynamics_constants)
-    Rᵛ  = vapor_specific_gas_constant(thermodynamics_constants)
+    cᵖv = vapor_heat_capacity(thermodynamics_constants)
+    Rᵛ  = vapor_gas_constant(thermodynamics_constants)
     Δc  = cᵖℓ - cᵖv
+    T⁺ = max(T, Tᵗʳ)
 
-    return pᵗʳ * (T / Tᵗʳ)^(Δc / Rᵛ) * exp((ℒ₀ - Δc * T₀) * (1/Tᵗʳ - 1/T) / Rᵛ)
+    return pᵗʳ * (T⁺ / Tᵗʳ)^(Δc / Rᵛ) * exp((ℒ₀ - Δc * T₀) * (1/Tᵗʳ - 1/T⁺) / Rᵛ)
 end
 
 @inline function saturation_specific_humidity(T, ρ, td)
     p★ = saturation_vapor_pressure(T, td)
-    Rᵛ = vapor_specific_gas_constant(td)
+    Rᵛ = vapor_gas_constant(td)
     return p★ / (ρ * Rᵛ * T)
 end
 
@@ -114,10 +120,11 @@ end
 # p → T
 # T → p
 
-struct MoistPrognosticState{FT}
+struct MoistState{FT}
     ρ :: FT
     θ :: FT
     q :: FT
+    z :: FT
 end
 
 struct MoistAuxiliaryState{FT}
@@ -126,23 +133,88 @@ struct MoistAuxiliaryState{FT}
     q :: FT
 end
 
-@inline function mixture_specific_gas_constant(q, thermo::AT)
-    Rᵈ = dry_air_specific_gas_constant(thermo)   
-    Rᵛ = vapor_specific_gas_constant(thermo)   
+@inline function specific_gas_constant(q, thermo::AT)
+    Rᵈ = dry_air_gas_constant(thermo)   
+    Rᵛ = vapor_gas_constant(thermo)   
     return Rᵈ * (1 - q) + Rᵛ * q
 end
 
-@inline mixture_specific_gas_constant(aux::MoistAuxiliaryState, thermo::AT) =
-    mixture_specific_gas_constant(aux.q, thermo)
-
-function density(aux::MoistAuxiliaryState, thermo)
-    Rᵐ = mixture_specific_gas_constant(aux, thermo)
-    return aux.p / (Rᵐ * aux.T)
+@inline function heat_capacity(q, thermo::AT)
+    cᵖᵈ = dry_air_heat_capacity(thermo)
+    cᵖᵛ = vapor_heat_capacity(thermo)
+    return cᵖᵈ * (1 - q) + cᵖᵛ * q
 end
 
-@inline function saturation_specific_humidity(aux::MoistAuxiliaryState, td)
-    ρ = density(aux, td)
-    p★ = saturation_vapor_pressure(aux.T, td)
-    Rᵛ = vapor_specific_gas_constant(td)
-    return p★ / (ρ * Rᵛ * aux.T)
+function density(T, p, q, thermo)
+    Rᵐ = specific_gas_constant(q, thermo)
+    return p / (Rᵐ * T)
+end
+
+# Over a liquid surface
+@inline function saturation_specific_humidity(T, p, q, td)
+    ρ = density(T, p, q, td)
+    p★ = saturation_vapor_pressure(T, td)
+    Rᵛ = vapor_gas_constant(td)
+    return p★ / (ρ * Rᵛ * T)
+end
+
+struct ReferenceState{FT}
+    p :: FT
+    θ :: FT
+end
+
+@inline function reference_pressure(z, ref, thermo)
+    cᵖᵈ = dry_air_heat_capacity(thermo)
+    Rᵈ = dry_air_gas_constant(thermo)
+    ϰᵈ = thermo.dry_air.isentropic_exponent
+    g = thermo.gravitational_acceleration
+    return ref.p * (1 - g * z / (cᵖᵈ * ref.θ))^ϰᵈ
+end
+
+@inline function exner_function(prog, ref, thermo)
+    Rᵐ = specific_gas_constant(prog.q, thermo)
+    cᵖᵐ = heat_capacity(prog.q, thermo)
+    inv_ϰᵐ = Rᵐ / cᵖᵐ
+    pᵣ = reference_pressure(prog.z, ref, thermo)
+    return (pᵣ / ref.p)^inv_ϰᵐ
+end
+
+# Solve
+# θ = T/Π ( 1 - ℒ max(0, q - q★) / (cᵖᵐ T))
+# root of: f(T) = T² - Π θ T - ℒ max(0, q - q★) / cᵖᵐ
+@inline function temperature(prog::MoistState{FT}, ref, thermo::AT) where FT
+    # Generate guess for unsaturated conditions
+    Π = exner_function(prog, ref, thermo)
+    T₁ = Π * prog.θ
+    q★ = saturation_specific_humidity(T₁, prog.ρ, thermo)
+    prog.q < q★ && return T₁
+
+    # Generate guess for saturated conditions
+    ℒ = thermo.condensation.reference_vaporization_enthalpy
+    cᵖᵐ = heat_capacity(prog.q, thermo)
+    qℓ⁺ = 1e-4 #max(0, prog.q - q★)
+    T₂ = T₁ + 1e-2 #sqrt(T₁^2 + 4 * ℒ * qℓ⁺ / cᵖᵐ) / 2
+
+    r₁ = adjustment_residual(T₁, Π, prog, thermo)
+    r₂ = adjustment_residual(T₂, Π, prog, thermo)
+
+    for i = 1:2
+        # Compute slope
+        dTdr = (T₂ - T₁) / (r₂ - r₁)
+        # Store
+        r₁ = r₂
+        T₁ = r₂
+        # Update
+        T₂ += r₂ * dTdr
+        r₂ = adjustment_residual(T₂, Π, prog, thermo)
+    end
+
+    return T₂
+end
+
+@inline function adjustment_residual(T, Π, prog, thermo)
+    q★ = saturation_specific_humidity(T, prog.ρ, thermo)
+    ℒ = thermo.condensation.reference_vaporization_enthalpy
+    cᵖᵐ = heat_capacity(prog.q, thermo)
+    return T^2 - Π * prog.θ * T - ℒ * max(0, prog.q - q★) / cᵖᵐ
 end
