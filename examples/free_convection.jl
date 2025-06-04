d8a10cb1fc12e721745755e84f25f3eeb4559b70
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-06-04 17:53:46 -0400

    add advection back

--- examples/free_convection.jl
+++ examples/free_convection.jl
@@ -34,7 +34,7 @@ q_bcs = FieldBoundaryConditions(bottom=vapor_flux)
 
 advection = WENO() #(momentum=WENO(), θ=WENO(), q=WENO(bounds=(0, 1)))
 tracers = (:θ, :q)
-model = NonhydrostaticModel(; grid, buoyancy,
+model = NonhydrostaticModel(; grid, advection, buoyancy,
                             tracers = (:θ, :q),
                             forcing = (; q=q_forcing),
                             boundary_conditions = (θ=θ_bcs, q=q_bcs))
