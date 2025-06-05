b0b55e798509370d6ef304c7871592aa54ce79aa
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-06-05 16:34:13 -0600

    updates

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,11 @@
+push!(LOAD_PATH,"../src/")
+
 using AquaSkyLES
 using Documenter
 
 DocMeta.setdocmeta!(AquaSkyLES, :DocTestSetup, :(using AquaSkyLES); recursive=true)
 
-makedocs(;
-    modules=[AquaSkyLES],
-    authors="Gregory Wagner",
-    repo="https://github.com/gregorywagner/AquaSkyLES.jl/blob/{commit}{path}#{line}",
-    sitename="AquaSkyLES.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://gregorywagner.github.io/AquaSkyLES.jl",
-        edit_link="main",
-        assets=String[],
-    ),
+makedocs(sitename="AquaSkyLES",
     pages=[
         "Home" => "index.md",
         "API" => "api.md",
